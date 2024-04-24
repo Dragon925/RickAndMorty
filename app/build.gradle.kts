9fd7a778f811ce5b9b5c8126be793fa918339c5c
Alexandr Romanov <37437841+Dragon925@users.noreply.github.com>
2024-04-24 12:10:20 +0300

    Готовы все странички приложения и начата работа над навигацией

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
 
     implementation(libs.fragment.ktx)
+    implementation(libs.bundles.navigation)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
